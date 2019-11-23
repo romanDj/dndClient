692a5783ebe084e3c444ca851a93c11db49358cd
romanDj <true-rock99@ya.ru>
2019-11-23 13:09:36 +0300

    edit sidebar client

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,15 +1,17 @@
 import React, {useState} from "react";
 import logo from "../statics/images/icon_ico.png";
+
 import {AiOutlineMenu} from "react-icons/ai";
 import {FaEllipsisV} from "react-icons/fa";
 import {IconContext} from "react-icons";
+import {GiDragonHead, GiPointyHat, GiSpellBook, GiSwordman, GiThreeFriends} from "react-icons/gi";
+import {TiHome} from "react-icons/ti";
 
 //верхняя навигационная панель
 function Navigation(props) {
     const [profileMenu, setProfileMenu] = useState(false);
-    const [sideMenu, setSideMenu] = useState(window.innerWidth > 560);
+    const [sideMenu, setSideMenu] = useState(window.innerWidth > 780);
 
-    //hooks, выбор вкладки, хранить текущую вкладку, и передавать соответствующй набор стилей
     let navbarItems = [
         {key: "page1", content: "Профиль", callback: () => console.log("собственное действие")},
         {key: "page10", content: "Персонажи"},
@@ -18,33 +20,40 @@ function Navigation(props) {
     ];
 
     let sideItems = [
-        {key: "page1", content: "Главная", callback: () => console.log("собственное действие"), select: true},
-        {key: "page2", content: "Заклинания"},
-        {key: "page6", content: "Магические предметы"},
-        {key: "page3", content: "Бестиарий"},
-        {key: "page100", content: "Создать персонажа"},
+        {
+            key: "page1",
+            content: "Главная",
+            icon: (<TiHome/>),
+            callback: () => console.log("собственное действие"),
+            select: true
+        },
+        {key: "page2", content: "Заклинания", icon: (<GiSpellBook/>)},
+        {key: "page6", content: "Магические предметы", icon: (<GiPointyHat/>)},
+        {key: "page3", content: "Бестиарий", icon: (<GiDragonHead/>)},
+        {key: "page100", content: "Создать персонажа", icon: (<GiSwordman/>)},
+        {key: "page120", content: "Галлерея персонажей", icon: (<GiThreeFriends/>)}
     ];
 
     return <>
         <div className="ui_navbar" onClick={() => (profileMenu === true && setProfileMenu(false))}>
-            <IconContext.Provider value={{style: {verticalAlign: 'middle'}}}>
-                <div className="ui_navbar_item" onClick={() => setSideMenu(!sideMenu)}>
+            <div className="ui_navbar_item" onClick={() => setSideMenu(!sideMenu)}>
+                <IconContext.Provider value={{style: {verticalAlign: 'middle'}}}>
                     <AiOutlineMenu/>
-                </div>
-            </IconContext.Provider>
+                </IconContext.Provider>
+            </div>
             <div className="bs_label">
                 <img src={logo} alt="Logo" className="bs_icon"/>
                 <p>Bs client</p>
             </div>
-            <IconContext.Provider value={{style: {verticalAlign: 'middle'}}}>
-                <div className="ui_navbar_item">
+            <div className="ui_navbar_item">
+                <IconContext.Provider value={{style: {verticalAlign: 'middle'}}}>
                     <FaEllipsisV onClick={() => setProfileMenu(!profileMenu)}/>
-                    <div className={profileMenu ? "ui_submenu-open" : "ui_submenu-close"}
-                         onClick={() => setProfileMenu(false)}>
-                        {ListItems(navbarItems, "ui_navbar_submenu_item")}
-                    </div>
+                </IconContext.Provider>
+                <div className={profileMenu ? "ui_submenu-open" : "ui_submenu-close"}
+                     onClick={() => setProfileMenu(false)}>
+                    {ListItems(navbarItems, "ui_navbar_submenu_item")}
                 </div>
-            </IconContext.Provider>
+            </div>
         </div>
 
         <div className="ui_sidebar_content" onClick={() => (profileMenu === true && setProfileMenu(false))}>
@@ -56,7 +65,6 @@ function Navigation(props) {
                 {props.children}
             </div>
         </div>
-
     </>
 }
 
@@ -68,7 +76,12 @@ function ListItems(items, classStyle, selectStyle = "") {
     return items.map(item => <div key={item.key}
                                   className={classStyle + (item.select ? (" " + selectStyle) : "")}
                                   onClick={() => (item.callback ? item.callback() : callback(item.key))}>
-        {item.content}</div>);
+        {item.icon != null &&
+        <IconContext.Provider value={{style: {fontSize: '25px', marginRight: '6px'}}}>
+            {item.icon}
+        </IconContext.Provider>}
+        <p>{item.content}</p>
+    </div>);
 }
 
 export default Navigation;
